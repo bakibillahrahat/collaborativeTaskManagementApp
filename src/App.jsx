a051d9a25282849b043d4fcd6b36a48fee459b4e
MD. Bakibillah Rahat <46396330+bakibillahrahat@users.noreply.github.com>
2023-09-08 03:10:36 +0600

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,27 @@
+// import Login from './components/Login';
+import { useState } from "react";
+import Signin from './components/Signin';
+import Signup from "./components/Signup";
 
+
+import './App.css'
 
 const App = () => {
+  const [currentForm, setCurrentForm] = useState('signin');
+  const toggleForm = (fornName) => {
+    setCurrentForm(fornName);
+  }
   return (
-    <div>App</div>
+    <main>
+      <div className="main">
+        <div className="gradient"></div>
+      </div>
+      <div className="app">
+        {
+          currentForm === 'signin' ? <Signin onFormSwitch={toggleForm}/> : <Signup onFormSwitch={toggleForm}/>
+        }
+      </div>
+    </main>
   )
 }
 
