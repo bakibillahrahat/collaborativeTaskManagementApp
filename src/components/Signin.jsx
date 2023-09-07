25b0657b5896d2da4f90d10adf2fcae04b0a8887
MD. Bakibillah Rahat <46396330+bakibillahrahat@users.noreply.github.com>
2023-09-08 03:54:49 +0600

    second commit

--- src/components/Signin.jsx
+++ src/components/Signin.jsx
@@ -1,5 +1,26 @@
+import { useState } from "react";
 
 const Login = (props) => {
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [message, setMessage] = useState('');
+
+
+    const handleLogin = () => {
+        const userData = localStorage.getItem(email);
+        const user = JSON.parse(userData)
+        if(email === '' || password === ''){
+            setMessage('Please fill up the form')
+        }else{
+            if(user.email === email && (password === user.password)){
+                setMessage('Login Successful');
+            }else{
+                setMessage('Invalid your credential');
+            }
+        }
+  
+    }
   return (
     <section id="signin">
         <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
@@ -10,11 +31,11 @@ const Login = (props) => {
                     <form action="#" className="space-y-4 md:space-y-6">
                         <div>
                             <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900">Your email</label>
-                            <input type="email" name="username" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="name@company.com" required=""/>
+                            <input type="email" name="username" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="name@company.com" required="" onChange={(e) => setEmail(e.target.value)}/>
                         </div>
                         <div>
                             <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900">Password</label>
-                            <input type="password" name="password" id="password" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="******" required=""/>
+                            <input type="password" name="password" id="password" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="******" required="" onChange={(e) => setPassword(e.target.value)}/>
                         </div>
                         <div className="flex items-center justify-between">
                             <div className="flex items-start">
@@ -27,8 +48,10 @@ const Login = (props) => {
                             </div>
                             <a href="#" className="text-sm font-medium text-primary-600 hover:underline">Forget password?</a>
                         </div>
-                        <button type="submit" className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Sign in</button>
-                        <p className="text-sm font-light text-gray-500 dark:text-gray-400">Don't have an account yeat? <button onClick={() => props.onFormSwitch('signup')} className="font-medium text-primary-600 hover:underline">Sign up</button></p>
+                        <button type="submit" className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center" onClick={handleLogin}>Sign in</button>
+                        <p className="text-sm font-light text-gray-500">Don't have an account yeat? <button onClick={() => props.onFormSwitch('signup')} className="font-medium text-primary-600 hover:underline">Sign up</button></p>
+
+                        <p className="text-sm font-light text-danger">{message}</p>
                     </form>
                 </div>
             </div>
--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 
+
 const Signup = (props) => {
 
     const [name , setName ] = useState('');
@@ -10,8 +11,8 @@ const Signup = (props) => {
     const [message, setMessage] = useState('');
 
     const handleSignup = () => {
-        const existingUser = localStorage.getItem(email);
 
+        // Create a new user object and store it in local storage
         const newUser = {
             name,
             email,
@@ -19,12 +20,18 @@ const Signup = (props) => {
             bio,
             avater
         }
-
-        if(existingUser){
-            setMessage('User already exists')
+        if(name === '' || email === '' || password === '' || bio === '' || avater === ''){
+            setMessage('Please fill up the form')
+        }else{
+            // check existing user
+            const existingUser = localStorage.getItem(email);
+            if(existingUser){
+                setMessage('User already exists');
+            }else{
+                localStorage.setItem(email, JSON.stringify(newUser));
+                setMessage('Signup successful! You can now Signin.')
+            }
         }
-
-        
     }
   return (
     <section>
@@ -36,24 +43,24 @@ const Signup = (props) => {
                     <form action="#" className="space-y-2 md:space-y-2">
                         <div>
                             <label htmlFor="name" className="block mb-2 text-sm font-medium text-gray-900">Your name</label>
-                            <input type="text" name="name" id="name" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="MD. " required=""/>
+                            <input type="text" name="name" id="name" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="MD. " required="" onChange={(e) => setName(e.target.value)}/>
                         </div>
                         <div>
                             <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900">Your email</label>
-                            <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="name@company.com" required=""/>
+                            <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="name@company.com" required="" onChange={(e) => setEmail(e.target.value)}/>
                         </div>
                         <div>
                             <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900">Password</label>
-                            <input type="password" name="password" id="password" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="******" required=""/>
+                            <input type="password" name="password" id="password" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="******" required="" onChange={(e) => setPassword(e.target.value)}/>
                         </div>
                         <div>
                             <label htmlFor="bio" className="block mb-2 text-sm font-medium text-gray-900">Bio</label>
-                            <textarea type="text" name="bio" id="bio" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="Tell something about your self" required=""/>
+                            <textarea type="text" name="bio" id="bio" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="Tell something about your self" required="" onChange={(e) => setBio(e.target.value)}/>
                         </div>
 
                         <div>
                             <label htmlFor="avater" className="block mb-2 text-sm font-medium text-gray-900">Avater</label>
-                            <input type="text" name="avater" id="avater" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="Avatar link" required=""/>
+                            <input type="text" name="avater" id="avater" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="Avatar link" required="" onChange={(e) => setAvater(e.target.value)}/>
                         </div>
 
                         <button type="submit" className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center" onClick={handleSignup}>Sign up </button>
